3fa6f540fd73ec7d47bf4018aed196daebac5fe4
nikita-maltsev <nikitasvi1000@gmail.com>
2023-09-25 18:36:06 +0300

    add commonJs and ngx notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,46 @@
 import { Component } from '@angular/core';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private toastr: ToastrService) {}
+
+  public toastr_alert(type: string) {
+    switch (type) {
+      case 'success':
+        this.toastr.success('Success Alert');
+        break;
+      case 'warning':
+        this.toastr.warning('Warning Alert');
+        break;
+      case 'error':
+        this.toastr.error('Error Alert');
+        break;
+      case 'info':
+        this.toastr.info('Info Alert');
+        break;
+    }
+  }
+
+  public notiflix_alert(type: string) {
+    switch (type) {
+      case 'success':
+        Notify.success('Success Alert');
+        break;
+      case 'warning':
+        Notify.warning('Warning Alert');
+        break;
+      case 'error':
+        Notify.failure('Error Alert');
+        break;
+      case 'info':
+        Notify.info('Info Alert');
+        break;
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
+
+import { ToastrModule, provideToastr } from 'ngx-toastr';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      progressBar: true,
+      progressAnimation: 'increasing',
+      positionClass: 'toast-bottom-right',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
