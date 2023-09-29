211971ace936054ed417e760e30e53cedeaf95fc
nikita-maltsev <nikitasvi1000@gmail.com>
2023-09-29 23:42:36 +0300

    add rxjs functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription, interval, map, scan, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public sequentialNumbers: number[] = [];
+  public randomNumbers: string[] = [];
+
+  private sequentialSubscription: Subscription | undefined;
+  private randomSubscription: Subscription | undefined;
+
+  private isSequentialRunning = false;
+  private isRandomRunning = false;
+
+  public ngOnInit(): void {
+    this.toggleSequential();
+    this.toggleRandom();
+  }
+
+  public toggleSequential() {
+    this.isSequentialRunning = !this.isSequentialRunning;
+
+    if (this.isSequentialRunning) {
+      this.sequentialSubscription = interval(2000)
+        .pipe(
+          takeWhile(() => this.isSequentialRunning),
+          scan((acc, _) => acc + 1, 0)
+        )
+        .subscribe((number) => {
+          this.sequentialNumbers.push(number);
+        });
+    } else {
+      this.sequentialSubscription?.unsubscribe();
+    }
+  }
+
+  public toggleRandom() {
+    this.isRandomRunning = !this.isRandomRunning;
+
+    if (this.isRandomRunning) {
+      this.randomSubscription = interval(2000)
+        .pipe(
+          takeWhile(() => this.isRandomRunning),
+          map(() => {
+            const randomValue = Math.floor(Math.random() * 1000);
+            return `Random Value: ${randomValue}`;
+          })
+        )
+        .subscribe((value) => {
+          this.randomNumbers.push(value);
+        });
+    } else {
+      this.randomSubscription?.unsubscribe();
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatListModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
